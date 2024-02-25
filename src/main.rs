b27a29ba14cda3f979b7d55a13ce4806e2feef83
Sayan Mallick <snyxmk@gmail.com>
2024-02-25 23:30:46 +0530

    tracing implemented

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use axum::{
-    http::{header::HeaderMap, StatusCode},
+    extract::MatchedPath,
+    http::{HeaderMap, Request, StatusCode},
     routing::{get, post},
     Json, Router,
 };
 
 use tokio_cron_scheduler::{Job, JobScheduler};
+use tower_http::trace::TraceLayer;
 
 use hermes::{Identity, PingResponse, SendRequest};
 use lettre::{
     message::header::ContentType, AsyncSmtpTransport, AsyncTransport, Message, Tokio1Executor,
 };
+use tracing::{info, info_span};
+
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 async fn send_email(send_request: SendRequest) {
     let email = Message::builder()
@@ -31,9 +36,32 @@ async fn send_email(send_request: SendRequest) {
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "hermes=debug,tower_http=debug,axum::rejection=trace".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let app = Router::new()
         .route("/ping", get(ping))
-        .route("/send", post(send));
+        .route("/send", post(send))
+        .layer(
+            TraceLayer::new_for_http().make_span_with(|request: &Request<_>| {
+                let matched_path = request
+                    .extensions()
+                    .get::<MatchedPath>()
+                    .map(MatchedPath::as_str);
+
+                info_span!(
+                    "http_request",
+                    method = ?request.method(),
+                    matched_path,
+                    some_other_field = tracing::field::Empty,
+                )
+            }),
+        );
 
     let scheduler = JobScheduler::new().await.unwrap();
 
@@ -41,20 +69,20 @@ async fn main() {
         .add(
             Job::new_async("1/10 * * * * *", |_uuid, _l| {
                 Box::pin(async move {
-                    let p = SendRequest {
-                        from: Identity {
-                            name: "Hermes".to_string(),
-                            email: "hermes@localhost".to_string(),
-                        },
-                        to: Identity {
-                            name: "Hermes".to_string(),
-                            email: "hermes@localhost".to_string(),
-                        },
-                        subject: "Hello".to_string(),
-                        body: "Hello".to_string(),
-                    };
-                    send_email(p).await;
-                    println!("send mail");
+                    // let p = SendRequest {
+                    //     from: Identity {
+                    //         name: "Hermes".to_string(),
+                    //         email: "hermes@localhost".to_string(),
+                    //     },
+                    //     to: Identity {
+                    //         name: "Hermes".to_string(),
+                    //         email: "hermes@localhost".to_string(),
+                    //     },
+                    //     subject: "Hello".to_string(),
+                    //     body: "Hello".to_string(),
+                    // };
+                    // send_email(p).await;
+                    info!("send mail");
                 })
             })
             .unwrap(),
@@ -63,10 +91,10 @@ async fn main() {
         .unwrap();
 
     scheduler.start().await.unwrap();
+    info!("scheduler started");
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:1111")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:1111").await.unwrap();
+    info!("listening on {}", listener.local_addr().unwrap());
 
     axum::serve(listener, app).await.unwrap();
 }
@@ -82,6 +110,6 @@ async fn send(headers: HeaderMap, Json(p): Json<SendRequest>) -> StatusCode {
     let apikey = headers.get("api-key").unwrap().to_str().unwrap();
     println!("body -> {p}");
     println!("api-key -> {apikey}");
-    send_email(p).await;
+    // send_email(p).await;
     StatusCode::OK
 }
