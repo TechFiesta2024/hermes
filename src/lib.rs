3adb37674bc5546cbc765883c6c2e5dd4bd21eee
Sayan Mallick <snyxmk@gmail.com>
2024-02-25 20:22:26 +0530

    basic async job

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use kafka::{
-  client::RequiredAcks,
-  producer::{Producer, Record},
+    client::RequiredAcks,
+    producer::{Producer, Record},
 };
 use serde::{Deserialize, Serialize};
 use std::error::Error;
@@ -9,38 +9,38 @@ use std::time::Duration;
 
 #[derive(Serialize)]
 pub struct PingResponse {
-  pub ok: bool,
-  pub msg: String,
+    pub ok: bool,
+    pub msg: String,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Identity {
-  pub name: String,
-  pub email: String,
+    pub name: String,
+    pub email: String,
 }
 
 impl Display for Identity {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    write!(f, "({}, {})", self.name, self.email)
-  }
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.name, self.email)
+    }
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct SendRequest {
-  pub from: Identity,
-  pub to: Identity,
-  pub subject: String,
-  pub body: String,
+    pub from: Identity,
+    pub to: Identity,
+    pub subject: String,
+    pub body: String,
 }
 
 impl Display for SendRequest {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    write!(
-      f,
-      "{} -> {}\nsubject: {}\nbody: {}",
-      self.from, self.to, self.subject, self.body
-    )
-  }
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{} -> {}\nsubject: {}\nbody: {}",
+            self.from, self.to, self.subject, self.body
+        )
+    }
 }
 
 // pub struct GlobalState {
