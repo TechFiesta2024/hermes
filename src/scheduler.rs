8bf1d05f026358b2b384cb20e2d6777170635931
Sayan Mallick <snyxmk@gmail.com>
2024-02-26 23:28:27 +0530

    added timezone

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -4,8 +4,11 @@ use tracing::info;
 use crate::db::get_data;
 use crate::email::{send_email, Email, Identity};
 
+// 30 * 12-18 Mar *
+// 30 minutes on everyday from 12 to 18 Mar
+
 pub fn job() -> Job {
-    Job::new_async("1/30 * * * * *", |_uuid, _l| {
+    Job::new_async_tz("1/30 * * * * *", chrono_tz::Asia::Kolkata, |_uuid, _l| {
         Box::pin(async move {
             info!("get data");
             let email = get_data().await.into_iter();
